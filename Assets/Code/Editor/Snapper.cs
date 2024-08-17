cbbffca5b3f95f1d9c851fec1d5291e6def453a7
Felix Lepistö <felix.lepisto@gmail.com>
2024-08-17 13:52:20 +0200

    Road colliders House<br />Main map

--- Assets/Code/Editor/Snapper.cs
+++ Assets/Code/Editor/Snapper.cs
@@ -2,11 +2,64 @@
 using UnityEditor;
 using UnityEngine;
 
+[CanEditMultipleObjects]
+[CustomEditor(typeof(SnapMarker))]
 public class Snapper : Editor
 {
+    private static float IncrementPos = 5f;
+    private static float IncrementRotation = 90;
+
+    private SerializedObject _this;
+    
+    private void OnEnable()
+    {
+        _this = new SerializedObject(this);
+    }
+
+    [MenuItem("Game/Rotate ^r")]
+    private static void Rotate()
+    {
+        var selected = Selection.GetFiltered<GameObject>(SelectionMode.TopLevel);
+        foreach (var obj in selected)
+        {
+            if (!obj.TryGetComponent<SnapMarker>(out _))
+            {
+                continue;
+            }
+            var pos = obj.transform.position;
+            pos.x = Mathf.Round(pos.x / IncrementPos) * IncrementPos;
+            pos.z = Mathf.Round(pos.z / IncrementPos) * IncrementPos;
+            obj.transform.position = pos;
+
+            var rotation = obj.transform.rotation.eulerAngles;
+            rotation.y = Mathf.Round(rotation.y / IncrementRotation) * IncrementRotation + IncrementRotation;
+            obj.transform.rotation = Quaternion.Euler(rotation);
+        }
+    }
+
     private void OnSceneGUI()
     {
-        //var selected = Selection.GetFiltered<GameObject>();
-        
+        var selected = Selection.GetFiltered<GameObject>(SelectionMode.TopLevel);
+        foreach (var obj in selected)
+        {
+            if (!obj.TryGetComponent<SnapMarker>(out _))
+            {
+                continue;
+            }
+            var pos = obj.transform.position;
+            pos.x = Mathf.Round(pos.x / IncrementPos) * IncrementPos;
+            pos.y = 0;
+            pos.z = Mathf.Round(pos.z / IncrementPos) * IncrementPos;
+            obj.transform.position = pos;
+
+            var rotation = obj.transform.rotation.eulerAngles;
+            rotation.y = Mathf.Round(rotation.y / IncrementRotation) * IncrementRotation;
+            obj.transform.rotation = Quaternion.Euler(rotation);
+        }
+    }
+
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.PropertyField(_this.FindProperty(nameof(IncrementPos)));
     }
 }
--- Assets/Code/SnapMarker.cs
+++ Assets/Code/SnapMarker.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class RoadMarker : MonoBehaviour
+public class SnapMarker : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
